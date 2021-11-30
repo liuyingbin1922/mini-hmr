8c23a097577d84dea2a7e66150fdba4bfdc68faa
刘颖斌 <liuyingbin@weidian.com>
2021-11-30 13:57:18 +0800

    feat: server

---
+++ src/myHMR-webpack-dev-server.js
@@ -0,0 +1,15 @@
+ // /src/myHMR-webpack-dev-server.js
+ 
+ const webpack = require("webpack");
+ const Server = require("./lib/server/Server");
+ const config = require("../../webpack.config");
+ 
+ // 【1】创建webpack实例 , 读取配置
+ const compiler = webpack(config);
+ // 【2】创建Server类，这个类里面包含了webpack-dev-server服务端的主要逻辑（在 2.Server整体 中会梳理他的逻辑）
+ const server = new Server(compiler);
+ 
+ // 最后一步【10】启动webserver服务器
+ server.listen(8000, "localhost", () => {
+     console.log(`Project is running at http://localhost:8000/`);
+ })
--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,13 @@ let HtmlWebpackPlugin = require("html-webpack-plugin")
 let path = require("path");
 module.exports = {
     mode: "development",
-    entry:"./src/index.js",// 这里我们还没有将客户端代码配置，而是通过updateCompiler方法更改entry属性
+    entry:{
+        main:[
+            "./src/index.js",// 这里我们还没有将客户端代码配置，而是通过updateCompiler方法更改entry属性
+            "./src/lib/client/index.js",
+            "./src/lib/client/hot/dev-server.js"
+        ]
+    },
     output: {
         filename: "[name].js",
         path: path.resolve(__dirname, "dist")
